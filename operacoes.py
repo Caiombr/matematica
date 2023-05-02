a483e1ec8b1979628a39586ab2a4f5c657b7c427
Caio Bittencourt <caiombr@gmail.com>
2023-05-02 17:13:12 -0300

    operações fundamentais da matemática

---
+++ operacoes.py
@@ -0,0 +1,14 @@
+def mais(x,y):
+    return x + y
+
+def menos(x,y):
+    return x - y
+
+def divisao(x,y):
+    return x/y
+
+def multiplicacao(x,y):
+    return x * y
+
+def potenciacao(x,y):
+    return x ** y
